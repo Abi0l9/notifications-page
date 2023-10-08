29e33bb93aca682acc1aaf06dc043b77daedfed1
Monsur Oyedeji <102850720+Abi0l9@users.noreply.github.com>
2023-10-08 09:18:46 +0100

    Update profile images

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,10 @@
 import type { INotifsRecord } from "./types";
 import { genRandomNumber } from "./utils";
 const IMG1 = "https://unsplash.com/photos/rDEOVtE7vOs/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fHx8MTY5NjczNTU3MXww&force=true&w=1920";
-const IMG2 = "../src/assets/dps/img2.jpg";
-const IMG3 = "../src/assets/dps/img3.jpg";
-const IMG4 = "../src/assets/dps/img4.jpg";
-const IMG5 = "../src/assets/dps/img5.jpg";
+const IMG2 = "https://unsplash.com/photos/c_GmwfHBDzk/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8M3x8cGVyc29ufGVufDB8fHx8MTY5NjczNTU3MXww&force=true&w=1920";
+const IMG3 = "https://unsplash.com/photos/QXevDflbl8A/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8cGVyc29ufGVufDB8fHx8MTY5NjczNTU3MXww&force=true&w=1920";
+const IMG4 = "https://unsplash.com/photos/mjRwhvqEC0U/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fHx8MTY5NjczNTU3MXww&force=true&w=1920";
+const IMG5 = "https://unsplash.com/photos/jmURdhtm7Ng/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8N3x8cGVyc29ufGVufDB8fHx8MTY5NjczNTU3MXww&force=true&w=1920";
 
 const records: INotifsRecord[] = [
   {
